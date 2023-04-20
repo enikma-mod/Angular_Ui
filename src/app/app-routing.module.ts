c207f3475c17f8f46724cb5112642bbdf22e46cf
enikma-mod <sirmodika@gmmail.com>
2023-04-20 21:19:55 +0200

    Updated Products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CartComponent } from './cart/cart.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
+  { path: 'nav', component: NavbarComponent },
   { path: '', component: HomepageComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'footer', component: FooterComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [
+  declarations: [			
     AppComponent,
     NavbarComponent,
-    HomepageComponent
-  ],
+    HomepageComponent,
+      ProductsComponent,
+      FooterComponent,
+      CartComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
