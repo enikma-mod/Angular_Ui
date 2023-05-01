5617045b2e08f5ed1d71e6bd20db4e017ede0601
enikma-mod <sirmodika@gmmail.com>
2023-05-01 13:42:27 +0200

    added the image cursor effect

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -13,3 +13,18 @@ export class CartComponent implements OnInit {
   }
 
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-cart-button',
+//   templateUrl: './cart-button.component.html',
+//   styleUrls: ['./cart-button.component.css']
+// })
+// export class CartButtonComponent {
+//   cartCount: number = 0;
+
+//   addToCart() {
+//     this.cartCount++;
+//   }
+// }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+changeImage(imageUrl: string) {
+  const img = document.getElementById('img-card') as HTMLImageElement;
+  img.src = imageUrl;
+}
 
   constructor() { }
 
